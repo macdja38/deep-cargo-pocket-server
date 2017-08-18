a7513a90a4ea64faf4603909b0002a721514e693
macdja38 <jakeincanada@icloud.com>
2017-08-18 03:11:37 -0400

    Add the ability to add custom tags and titles

--- routes/pocket.js
+++ routes/pocket.js
@@ -8,21 +8,48 @@ router.get('/', function (req, res, next) {
   res.send('index');
 });
 
+/**
+ * Calculates array of urls from input string
+ * @param {string} url
+ * @param {string} action
+ * @param {string} [tags]
+ * @param {string} [title]
+ * @returns {[{action: string, url: string, tag: string, title: string}]}
+ */
+function getActions(url, action, tags, title) {
+  let match = url.match(/{(\d+)-(\d+)}/);
+  let urls;
+  if (!match) {
+    urls = [{url, action, tags, title}];
+  } else {
+    let smallNum = parseInt(match[1], 10);
+    let bigNum = parseInt(match[2], 10);
+    urls = [];
+    for (let i = smallNum; i <= bigNum; i++) {
+      const actionInstance = {url: url.replace(/{\d+-\d+}/, i), action};
+      if (tags) {
+        actionInstance.tags = tags;
+      }
+      if (title) {
+        actionInstance.title = `${title} - ${i}`;
+      }
+      urls.push(actionInstance)
+    }
+  }
+  return urls;
+}
+
 router.post('/add', function (req, res, next) {
   console.log(req.body);
-  let urls = req.body.urls;
+  let rawURL = req.body.urls;
+
+  const actions = getActions(rawURL, "add", req.body.tags, req.body.title);
+
   const pocket = new Pocket({consumer_key: config.POCKET_CONSUMER_KEY, access_token: req.user.accessToken});
 
-  const actions = urls.map(url => ({action: 'add', url: url}));
+  console.log("actions", actions);
 
-  console.log(actions);
-  const actionString = JSON.stringify(actions);
-
-  console.log(actionString);
-
-  console.log(encodeURIComponent(actionString));
-  // pocket.add({url: "http://shiroyukitranslations.com/ztj-chapter-130/"}).then(console.log).catch(console.error);
-  pocket.modify({actions: urls.map(url => ({action: 'add', url: url}))}).then(result => {
+  pocket.modify({actions}).then(result => {
     console.log(result);
     res.json(result)
   }).catch((error) => {
