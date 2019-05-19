28fe246d5c7679dfabe392bd831c82448e0698f6
Jacob <jakeincanada@icloud.com>
2019-05-19 22:19:08 +0900

    Add sentry URL to config

--- config.example.js
+++ config.example.js
@@ -3,5 +3,6 @@ module.exports = {
   CLIENT_URL: "http://localhost:3000",
   ORIGINS: ["http://127.0.0.1:3000", "https://127.0.0.1:3000", "http://localhost:3000", "https://localhost:3000"],
   SECRET: "keyboard cat",
-  RETHINKDB: {host: 'localhost', port: 28015}
+  SENTRY_KEY: "https://e5c9202afa3f4b788ce062039b67a4f2@sentry.dvse.me/11",
+  RETHINKDB: {host: 'localhost', port: 28015},
 };
