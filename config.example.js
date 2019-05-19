00104e17a4c1a1e8cc27f49de7a3ac94635214cc
macdja38 <jakeincanada@icloud.com>
2019-05-19 23:02:45 +0900

    Add sentry integration

--- config.example.js
+++ config.example.js
@@ -3,6 +3,6 @@ module.exports = {
   CLIENT_URL: "http://localhost:3000",
   ORIGINS: ["http://127.0.0.1:3000", "https://127.0.0.1:3000", "http://localhost:3000", "https://localhost:3000"],
   SECRET: "keyboard cat",
-  SENTRY_KEY: "https://e5c9202afa3f4b788ce062039b67a4f2@sentry.dvse.me/11",
-  RETHINKDB: {host: 'localhost', port: 28015},
+  SENTRY_DSN: "https://e5c9202afa3f4b788ce062039b67a4f2@sentry.dvse.me/11",
+  RETHINKDB: {host: 'localhost', port: 28015, buffer: 2},
 };
